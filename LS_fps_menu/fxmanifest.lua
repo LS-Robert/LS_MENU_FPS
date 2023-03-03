38bf60f792f4d2e117ee4be26da328ca135556db
Roberto <78977643+LS-Robert@users.noreply.github.com>
2023-03-03 19:04:13 +0100

    Add files via upload

---
+++ LS_fps_menu/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Anto Orza#6115"
+description "Ls FPS Menu"
+version "1.0.0"
+
+client_scripts {
+        'client.lua'
+}
+
+server_scripts {
+        'server.lua'
+}
