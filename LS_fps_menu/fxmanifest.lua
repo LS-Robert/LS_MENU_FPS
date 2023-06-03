a37a6dac68d7a2a421549f3e21ae602c8e0ce941
Roberto <78977643+LS-Robert@users.noreply.github.com>
2023-06-03 19:10:55 +0200

    Update fxmanifest.lua

--- LS_fps_menu/fxmanifest.lua
+++ LS_fps_menu/fxmanifest.lua
@@ -1,9 +1,21 @@
-fx_version "cerulean"
-game "gta5"
+--[[
+ _       _____ 
+| |     / ____|
+| |    | (___
+| |     \___ \
+| |____ ____) |
+|______|_____/
 
-author "Anto Orza#6115"
-description "Ls FPS Menu"
-version "1.0.0"
+]]
+                                 
+                                 
+fx_version 'cerulean'
+game 'gta5'
+
+version '1.0.0'
+author 'Anto Orza#6115'
+description 'LS FPS Menu (https://orzafamily.com/)'
+repository 'https://github.com/LS-Robert/LS_MENU_FPS'
 
 shared_script 'config.lua'
 
